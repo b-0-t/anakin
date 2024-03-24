650031ba5d11ec962e044bb6eae878222c1906fe
dhruv <dhruvjn110327>
2024-03-24 14:16:20 +0530

    access denied

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup as bs
 import requests 
 
+
+# url of all addresses
 url = "https://www.enterprise.com/en/car-rental-locations.html?icid=header.locations.car.locations-_-international.locations-_-ENUS.NULL"
 
 
