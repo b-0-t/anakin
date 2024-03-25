9e1d644dd93b230a5ea29881f18a62cb14248b7f
dhruv <dhruvjn110327>
2024-03-26 01:16:40 +0530

    scarpping countries

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from bs4 import BeautifulSoup as bs
 import requests 
 import pandas as pd
 
-# url of all addresses
-url = "https://www.enterprise.com/en/car-rental-locations.html?icid=header.locations.car.locations-_-international.locations-_-ENUS.NULL"
+# url for getting all countries
+url_countries = "https://www.enterprise.com/en/car-rental-locations.html?icid=header.locations.car.locations-_-international.locations-_-ENUS.NULL"
+
+# setting up the right header to avoid blocking
+header = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"}
+
+r= requests.get(url_countries,headers=header)
+
+if(r.status_code==200):
+    soup = bs(r.content,"html.parser")
+    # print(soup)
+else:
+    print("Failed to fetch page")
 
 
-# lables=pd.read_html(url)
-# print(lables)
 
-# setting up the right header 
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"}
+divs_with_countryList = soup.find_all('div',class_='list link-list-band--4-column aem-GridColumn aem-GridColumn--default--12')
 
-r= requests.get(url,headers=headers)
-soup = bs(r.content,"html.parser")
-print(soup)
+countries=[]
+for spans in range(1,len(divs_with_countryList)):
+    divs_with_countryList[spans].find_all('span').text
+
+
+
+print(countries)
